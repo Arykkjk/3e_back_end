8134a3e5c9c52ec6079236fa0ca3bb343a7346e5
Arykkjk <aryella.batschauer@escola.pr.gov.br>
2025-02-17 10:46:57 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 import pandas as pd
-from PyQt5.QtWidgets import QAbstractButton, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QGridLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QGridLayout, QMessageBox
 from PyQt5.QtCore import Qt
 import smtplib
 from email.mime.text import MIMEText
@@ -21,7 +21,7 @@ class CadastroAluno(QWidget):
         self.setLayout(layout)
 
         label_nome = QLabel('Nome do aluno:')
-        self.input_nome = QlineEdit()
+        self.input_nome = QLineEdit()
         layout.addWidget(self.input_nome, 0, 0)
         layout.addWidget(self.input_nome, 0, 1)
 
@@ -35,5 +35,64 @@ class CadastroAluno(QWidget):
         layout.addWidget(label_email, 2, 0)
         layout.addWidget(self.input_email, 2, 1)
 
-        botao_adicionar
+        botao_adicionar = QPushButton('Adicionar aluno')
+        botao_adicionar.clicked.connect(self.adicionar_aluno)
+        layout.addWidget(botao_adicionar, 3, 0, 1, 2)
 
+        botao_cadastrar = QPushButton('Cadastrar e enviar emails')
+        botao_cadastrar.clicked.connect(self.cadastar_e_enviar_emails)
+        layout.addWidget(botao_cadastrar,4, 0, 1, 2)
+
+        self.remetente = "concursopilargames@gmail.com"
+        self.senha = "@PilarMsturana2"
+
+        def adicionar_aluno(self):
+            nome = self.input_nome.text()
+            turma = self.input_turma.text()
+            email = self.input_email.text()
+
+            if nome and turma and email:
+                self.alunos.append({'Nome': nome, 'Turma': turma, 'Email': email})
+                self.input_nome.clear()
+                self.input_turma.clear()
+                self.input_email.clear()
+            else:
+                QMessageBox.warning(self, 'Erro, Preencha todos os campo!')
+
+        def cadastrar_e_enviar_emails(self):
+            if self.alunos:
+                df = pd.DataFrame(self.alunos)
+                df.to_excel("cadastro_alunos.xlsx", index=False)
+
+                for aluno in self.alunos:
+                   self.enviar_email(aluno['Email'])
+
+                QMessageBox.infotmation(self, 'Sucesso', 'Cadastro completo e emais enviados!')
+            else:
+                QMessageBox.warning(self, 'Erro', 'Nenhum aluno cadastrado!')
+
+            def enviar_email(self, destinatario):
+                msg = MIMEMultipart()
+                msg['From'] = self.remetente
+                msg['To'] = destinatario
+                msg['Subject'] = "Confitmação de Inscrição - Concurso de Jogos"
+
+                corpo = "Olá, sua inscrição no concurso de jogos foi confirmada!"
+                msg.attach(MIMEText(corpo, 'plain'))
+
+                servidor = smtplib.SMTP('smtp.com', 587)
+                servidor.starttls()
+                servidor.login(self.remetente, self.senha)
+                texto = msg.as_string()
+                servidor.sendmail(self.remetente, destinatario. texto)
+                servidor.quit()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = CadastroAluno()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+                
+        
